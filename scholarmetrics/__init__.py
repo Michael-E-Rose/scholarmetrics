16d6f3b149016ca9ce0da4ba3fa46069d67394ff
Michael-E-Rose <Michael.Ernst.Rose@gmail.com>
2017-07-27 18:40:44 +0200

    Switch to PBR

--- scholarmetrics/__init__.py
+++ scholarmetrics/__init__.py
@@ -4,9 +4,10 @@ __author__ = """Michael E. Rose"""
 __email__ = 'Michael.Ernst.Rose@gmail.com'
 __all__ = ['euclidean', 'gindex', 'hindex']
 
+from pbr.version import VersionInfo
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+_v = VersionInfo('scholarmetrics').semantic_version()
+__version__ = _v.release_string()
+version_info = _v.version_tuple()
 
 from .metrics import *
