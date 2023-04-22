f1baddae6ed7db3b37c39cb89925cc74437c0112
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2023-04-22 19:28:39 +0200

    Fix version import

--- scholarmetrics/__init__.py
+++ scholarmetrics/__init__.py
@@ -1,13 +1,14 @@
-# -*- coding: utf-8 -*-
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib.metadata import version
+else:
+    from importlib_metadata import version
+
+__version__ = version("scholarmetrics")
 
 __author__ = """Michael E. Rose"""
 __email__ = 'Michael.Ernst.Rose@gmail.com'
 __all__ = ['euclidean', 'gindex', 'hindex']
-
-from pbr.version import VersionInfo
-
-_v = VersionInfo('scholarmetrics').semantic_version()
-__version__ = _v.release_string()
-version_info = _v.version_tuple()
 
 from .metrics import *
